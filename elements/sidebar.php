e9abc1d148acdfa18677da4860735c7543282660
Mario <mariovidovic97@gmail.com>
2023-02-23 09:25:18 +0100

    Init commit

--- elements/sidebar.php
+++ elements/sidebar.php
@@ -1,10 +1,11 @@
 <nav id="sidebar">
     <div class="sidebar_title">
-        <img src="./assets/logo.svg" />
-        <h4>mario-dev</h4>
+        <img src="./assets/logo2.svg" />
+        <h4>Company</h4>
     </div>
     <div class="sidebar_content">
         <a href="./" <?php if (basename($_SERVER['PHP_SELF']) == 'index.php'){echo 'class="active"';} ?>>Dashboard</a>
+        <hr/>
         <a href="./clients" <?php if (basename($_SERVER['PHP_SELF']) == 'clients.php'){echo 'class="active"';} ?>>Clients</a>
 </div>
 </nav>
