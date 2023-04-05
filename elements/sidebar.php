3c5ccd68067759fdb41ac4d440f1e625536680f9
Mario <mariovidovic97@gmail.com>
2023-04-05 09:25:18 +0200

    Sidebar updated

--- elements/sidebar.php
+++ elements/sidebar.php
@@ -4,10 +4,10 @@
         <h4>Company</h4>
     </div>
     <div class="sidebar_content">
-        <a href="./" <?php if (basename($_SERVER['PHP_SELF']) == 'index.php'){echo 'class="active"';} ?>>Dashboard</a>
+        <a href="./" <?php if (basename($_SERVER['PHP_SELF']) == 'index.php'){echo 'class="active"';} ?>><img src="./assets/icons/home.svg" />Dashboard</a>
         <hr/>
-        <a href="./invoices" <?php if (basename($_SERVER['PHP_SELF']) == 'invoices.php'){echo 'class="active"';} ?>>Invoices</a>
-        <a href="./clients" <?php if (basename($_SERVER['PHP_SELF']) == 'clients.php'){echo 'class="active"';} ?>>Clients</a>
-        <a href="./transactions" <?php if (basename($_SERVER['PHP_SELF']) == 'transactions.php'){echo 'class="active"';} ?>>Transactions</a>
+        <a href="./invoices" <?php if (basename($_SERVER['PHP_SELF']) == 'invoices.php'){echo 'class="active"';} ?>><img src="./assets/icons/invoice.svg" />Invoices</a>
+        <a href="./clients" <?php if (basename($_SERVER['PHP_SELF']) == 'clients.php'){echo 'class="active"';} ?>><img src="./assets/icons/clients.svg" />Clients</a>
+        <a href="./transactions" <?php if (basename($_SERVER['PHP_SELF']) == 'transactions.php'){echo 'class="active"';} ?>><img src="./assets/icons/wallet.svg" />Transactions</a>
 </div>
 </nav>
